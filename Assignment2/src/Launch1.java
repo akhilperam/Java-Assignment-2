e77ffcd6b7b89640a0268fcbdfe257d5c7e8288e
akhilperam <akhiakhil96@gmail.com>
2023-09-30 14:54:46 +0530

    Initial Commit

---
+++ Assignment2/src/Launch1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Launch1 {
+
+	// 1. WAP to find the duplicates present in an array.
+	//Ex: {1 1 2 2 3 4 5 6 7}  
+//	       1 2
+		public static void main(String[] args)
+		{
+	        Scanner sc= new Scanner(System.in);
+	        int arr[]=new int[9];
+	        int ans=0;
+	        
+	        for(int i=0;i<arr.length;i++)
+	        {
+	        	arr[i]=sc.nextInt();
+	        }
+	        
+	        for(int i=0;i<arr.length;i++)
+	        {
+	        	for(int j=i+1;j<arr.length;j++)
+	        	{
+	        		if(arr[i]==arr[j])
+	        		{
+	        			ans=arr[j];
+	        			System.out.print(ans+" ");
+	        		}
+	        	}
+	        	
+	        }
+	        
+
+		}
+
+	}
